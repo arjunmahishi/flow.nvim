44a1fc4267f849eccd2c5713ccbb560eb2a43c7e
Arjun Mahishi <arjun.mahishi@gmail.com>
2021-11-17 00:14:50 +0530

    add support for more languages

--- lua/run-code/cmd.lua
+++ lua/run-code/cmd.lua
@@ -1,5 +1,11 @@
+-- TODO: do a check to see if these commands are available
 lang_cmd_map = {
-  lua = "lua <<-EOF\n%s\nEOF"
+  lua = "lua <<-EOF\n%s\nEOF",
+  -- python = "python3 <<-EOF\n%s\nEOF",
+  ruby = "ruby <<-EOF\n%s\nEOF",
+  bash = "bash <<-EOF\n%s\nEOF",
+  sh = "sh <<-EOF\n%s\nEOF",
+  scheme = "scheme <<-EOF\n%s\nEOF" --TODO: try to clean up the output
 }
 
 function cmd(block)
