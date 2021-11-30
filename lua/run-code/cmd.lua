49dd9a81fdcb48697c91f581f3552ff52a4d4a38
Arjun Mahishi <arjun.mahishi@gmail.com>
2021-11-30 20:14:51 +0530

    Simplify code execution with vim.fn.system

--- lua/run-code/cmd.lua
+++ lua/run-code/cmd.lua
@@ -1,27 +1,26 @@
 -- TODO: validate if these commands are available
 lang_cmd_map = {
-  lua = "lua %s <<-EOF\n%s\nEOF",
-  python = "python %s <<-EOF\n%s\nEOF",
-  ruby = "ruby %s <<-EOF\n%s\nEOF",
-  bash = "bash %s <<-EOF\n%s\nEOF",
-  sh = "sh %s <<-EOF\n%s\nEOF",
-  scheme = "scheme %s <<-EOF\n%s\nEOF" --TODO: try to clean up the output
+  lua = "lua <<-EOF\n%s\nEOF",
+  python = "python <<-EOF\n%s\nEOF",
+  ruby = "ruby <<-EOF\n%s\nEOF",
+  bash = "bash <<-EOF\n%s\nEOF",
+  sh = "sh <<-EOF\n%s\nEOF",
+  scheme = "scheme <<-EOF\n%s\nEOF" --TODO: try to clean up the output
 }
 
 -- constructs a command in the following format:
 --
--- <binary to run> 2> <output_file> 1>> <output_file> <<-EOF
+-- <binary to run> <output_file> <<-EOF
 --    <code>
 -- EOF
 --
 function cmd(lang, code, output_file)
-  local redirect_str = string.format("2> %s 1>> %s", output_file, output_file)
   local cmd_tmpl = lang_cmd_map[lang]
   if cmd_tmpl == nil then
     return ""
   end
 
-  return string.format(cmd_tmpl, redirect_str, code)
+  return string.format(cmd_tmpl, code)
 end
 
 return {
--- lua/run-code/execute.lua
+++ lua/run-code/execute.lua
@@ -8,38 +8,7 @@ function execute(lang, code)
     return
   end
 
-  run_and_print(c)
-end
-
--- helper functions to execute a command and print its output
-
-function file_exists(file)
-  local f = io.open(file, "rb")
-  if f then f:close() end
-  return f ~= nil
-end
-
-function read_file(file)
-  if not file_exists(file) then return {} end
-
-  local lines = {}
-  for line in io.lines(file) do
-    lines[#lines + 1] = line
-  end
-
-  return lines
-end
-
--- run and print the output by reading the file the output was piped to.
--- the error need not explictly be handled since it is already being piped
--- to the same output file
-function run_and_print(cmd)
-  os.execute(cmd)
-
-  local lines = read_file(output_file)
-  for k, v in pairs(lines) do
-    print(v)
-  end
+  print(vim.fn.system(c))
 end
 
 return {
